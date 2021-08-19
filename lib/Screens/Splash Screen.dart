60a75efb8b3c609c245ade8575ed05971c2f2aa3
Sachitbansal <44609504+Sachitbansal@users.noreply.github.com>
2021-08-19 14:08:15 +0530

    Splash Screen Logo

--- lib/Screens/Splash Screen.dart
+++ lib/Screens/Splash Screen.dart
@@ -10,7 +10,7 @@ class SplashScreen extends StatelessWidget {
       body: AnimatedSplashScreen(
         splash: Container(
           child:
-          Center(child: Image.network('https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_e2400d766f2c932321a026d6355d0874/agropro.png',height: 150,),),
+          Center(child: Image.network('https://i.ibb.co/SVmqq8d/Logo.jpg',height: 150,),),
         ), nextScreen: HomePage(),
         splashTransition: SplashTransition.slideTransition,
         // pageTransitionType: PageTransitionType.scale,
